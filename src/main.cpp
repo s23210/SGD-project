83ea89060eda7624b561e3d56c379c5749261c8d
s23210 <s23210@pjwstk.edu.pl>
2023-05-07 16:25:50 +0200

    Limit FPS

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,35 @@
+#include <string>
 #include "GameLoop.h"
 GameLoop* g = new GameLoop();
 
+const int TICKS = 10;
+
 int main(int argc, char* argv[]) {
     g->Initialize();
+    auto prev_tick = SDL_GetTicks();
+    int frame_dropped = 0;
     while (g->getIsRunning()) {
-        g->Render();
-        g->Event();
+//        Uint64 start = SDL_GetPerformanceCounter();
+        if (!frame_dropped) {
+            g->Render();
+            g->Event();
+            g->Update();
+        }
+
+        auto curr_tick = SDL_GetTicks();
+
+        if (curr_tick - prev_tick < TICKS) {
+            SDL_Delay(TICKS - (curr_tick - prev_tick));
+            frame_dropped = 0;
+        } else {
+            frame_dropped = 1;
+        }
+
+        prev_tick += TICKS;
+
+//        Uint64 end = SDL_GetPerformanceCounter();
+//        float elapsedMS = (end - start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
+//        SDL_Delay(floor(16.666f - elapsedMS));
     }
     g->Clear();
     return 0;
